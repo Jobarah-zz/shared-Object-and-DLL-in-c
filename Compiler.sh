c515c9cc26cabbcf54ac12fd67110fa499bc0e36
Josue Enamorado <jonamorado@joenamorado.local>
2016-12-16 15:14:51 -0600

    Changes made to the Compiler.sh

--- Compiler.sh
+++ Compiler.sh
@@ -1,6 +1,6 @@
 gcc -c -Wall -Werror -fpic Adder.c
 gcc -shared -o libAdder.so Adder.o
 gcc -L$(pwd) -Wall -o test MainSum.c -lAdder
-LD_LIBRARY_PATH=/home/username/foo:$LD_LIBRARY_PATH
-export LD_LIBRARY_PATH=/home/username/foo:$LD_LIBRARY_PATH
+LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$(pwd):$LD_LIBRARY_PATH
 ./test
